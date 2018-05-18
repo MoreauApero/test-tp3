8c17bccb2c92ec5542b230302045314ad254ca7e
Victor Moreau <21606600@campus.unicaen.fr>
2018-05-18 14:36:10 +0200

    Add models
    models classes generation

--- app/config/config.php
+++ app/config/config.php
@@ -1,30 +1,42 @@
 <?php
 return array(
-		"siteUrl"=>"http://127.0.0.1/firstProject/",
-		"database"=>[
-				"type"=>"mysql",
-				"dbName"=>"",
-				"serverName"=>"127.0.0.1",
-				"port"=>"3306",
-				"user"=>"root",
-				"password"=>"",
-				"options"=>[],
-				"cache"=>false
-		],
-		"sessionName"=>"firstProject",
-		"namespaces"=>[],
-		"templateEngine"=>'Ubiquity\\views\\engine\\Twig',
-		"templateEngineOptions"=>array("cache"=>false),
-		"test"=>false,
-		"debug"=>false,
-		"di"=>["jquery"=>function($controller){
+	"siteUrl"=>"http://127.0.0.1/firstProject/",
+	"database"=>array(
+			"type"=>"mysql",
+			"dbName"=>"messagerie",
+			"serverName"=>"127.0.0.1",
+			"port"=>3306,
+			"user"=>"root",
+			"password"=>"",
+			"options"=>array(),
+			"cache"=>false
+			),
+	"sessionName"=>"firstProject",
+	"namespaces"=>array(),
+	"templateEngine"=>"Ubiquity\\views\\engine\\Twig",
+	"templateEngineOptions"=>array(
+			"cache"=>false
+			),
+	"test"=>false,
+	"debug"=>false,
+	"di"=>array(
+			"jquery"=>function ($controller){
 							$jquery=new \Ajax\php\ubiquity\JsUtils(["defer"=>true],$controller);
 							$jquery->semantic(new \Ajax\Semantic());
 							return $jquery;
-						}],
-		"cache"=>["directory"=>"cache/","system"=>"Ubiquity\\cache\\system\\ArrayCache","params"=>[]],
-		"mvcNS"=>["models"=>"models","controllers"=>"controllers","rest"=>""],
-		"isRest"=>function(){
+						}
+			),
+	"cache"=>array(
+			"directory"=>"cache/",
+			"system"=>"Ubiquity\\cache\\system\\ArrayCache",
+			"params"=>array()
+			),
+	"mvcNS"=>array(
+			"models"=>"models",
+			"controllers"=>"controllers",
+			"rest"=>""
+			),
+	"isRest"=>function (){
 			return \Ubiquity\utils\http\URequest::getUrlParts()[0]==="rest";
 		}
-);
+	);
